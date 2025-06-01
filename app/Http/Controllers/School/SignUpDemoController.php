fb576df4a08c094705853d8838f12a02536536ef
shakya77 <bijanshakya145@gmail.com>
2025-06-01 23:27:47 +0545

    made api of signup demo for my react project of school

--- app/Http/Controllers/School/SignUpDemoController.php
+++ app/Http/Controllers/School/SignUpDemoController.php
@@ -3,14 +3,44 @@
 namespace App\Http\Controllers\School;
 
 use App\Http\Controllers\Controller;
+use App\Models\SchoolDemo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SignUpDemoController extends Controller
 {
     public function store()
     {
-        return response()->json([
-            'message' => 'Demo signup successful. We will contact you soon.'
-        ], 201);
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'schoolName' => 'required|string|max:255',
+            'phone' => 'required|string|max:20',
+            'message' => 'nullable|string|max:500',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["success" => false, "message" => $validator->errors()->all()], 200);
+        }
+
+        $school_demo = new SchoolDemo();
+        $school_demo->full_name = request()->name;
+        $school_demo->email = request()->email;
+        $school_demo->school_name = request()->schoolName;
+        $school_demo->phone = request()->phone;
+        $school_demo->message = request()->message;
+        $school_demo->save();
+        if ($school_demo->save()) {
+            return response()->json([
+                'success' => true,
+                'header' => 'Thank You!',
+                'message' => 'Demo signup successful. We will contact you soon.'
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Demo signup failed. Please try again.'
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\School\SignUpDemoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/school/demo', [SignUpDemoController::class, 'store'])->name('school.demo.store');
+Route::post('/school/demo', [SignUpDemoController::class, 'store'])->name('school.demo.store');
